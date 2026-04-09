1abc38429a99337d31406dd5db7a4f078d3d2159
Mohan K R <mohankr211@gmail.com>
2026-04-09 12:37:40 +0530

    Updated files before switching branch

--- app.py
+++ app.py
@@ -13,13 +13,17 @@ st.title("AI Email Writer")
 
 purpose = st.text_input("Enter Purpose")
 tone = st.selectbox("Select Tone", ["Formal", "Friendly", "Apology", "Request"])
+language = st.selectbox("Select Language", ["English", "Spanish", "French", "German","Chinese","kannada","Hindi"])
+othher_language = st.text_input("if other language selected, please specify here")
+
 
 points = st.text_area("Enter Key Points")
 
 if st.button("Generate Email"):
 
     prompt = f"""
-    Write an email based on:
+    Write an email ONLY in {language}
+    otrher language: {othher_language}
 
     Purpose: {purpose}
     Tone: {tone}
@@ -30,6 +34,9 @@ if st.button("Generate Email"):
     2. Follow-up Email
     3. Short Version
     4. 5 Subject Lines
+
+    The entire response must be in {language}
+other language: {othher_language}
     """
 
     response = client.chat.completions.create(
